73f114b68e21a479472754fbb6d56bf141826c92
IvanWork <ivanwatester2@gmail.com>
2022-12-29 08:03:03 +0500

    I could do it! I did possible to choose language for the page.

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
-from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.common.by import By
+# from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -8,3 +9,11 @@ def test_guest_can_go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
     time.sleep(5)
+
+
+# def test_guest_can_go_to_login_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     page = MainPage(browser, link)
+#     page.open()
+#     page.go_to_login_page()
+#     time.sleep(5)
