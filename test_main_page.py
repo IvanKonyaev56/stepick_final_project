40c61f4c87504ec504305cd7e5cdfdae78a70c4f
IvanWork <ivanwatester2@gmail.com>
2022-12-29 08:23:46 +0500

    Module 4.2 - lesson 6. Added error handling.

--- test_main_page.py
+++ test_main_page.py
@@ -1,19 +1,16 @@
 import time
-from selenium.webdriver.common.by import By
-# from .pages.main_page import MainPage
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-    time.sleep(5)
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
 
 
-# def test_guest_can_go_to_login_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.go_to_login_page()
-#     time.sleep(5)
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
