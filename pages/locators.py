781da144e3aa53a419132baae751e92835eeb0e0
IvanKonyaev56 <ivanwatester2@gmail.com>
2023-01-07 07:52:12 +0500

    Before global changes.

--- pages/locators.py
+++ pages/locators.py
@@ -1,5 +1,9 @@
 from selenium.webdriver.common.by import By
 
 
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "[name=login_submit]")
+    REGISTER_FORM = (By.CSS_SELECTOR, "[name=registration_submit]")
+
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
