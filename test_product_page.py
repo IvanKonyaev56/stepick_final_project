ee5321bd8cf9f3ef1180682cbc311406b305df63
IvanKonyaev56 <ivanwatester2@gmail.com>
2023-02-10 09:54:57 +0500

    Раздел 4 Урок 4.3. Задание №8.

--- test_product_page.py
+++ test_product_page.py
@@ -22,25 +22,37 @@ from pages.product_page import ProductPage
 #     page.message_about_added_to_basket()
 #     page.message_about_cost_of_basket()
 #     # time.sleep(10)
+#
+# @pytest.mark.xfail(reason="Негативная проверка")
+# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+#     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.take_the_goods_to_basket()
+#     page.should_not_be_success_message()
+#
+# def test_guest_cant_see_success_message(browser):
+#     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.should_not_be_success_message()
+#
+# @pytest.mark.xfail(reason="Негативная проверка")
+# def test_message_disappeared_after_adding_product_to_basket(browser):
+#     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.take_the_goods_to_basket()
+#     page.should_be_success_message()
 
-@pytest.mark.xfail(reason="Негативная проверка")
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
-    page.take_the_goods_to_basket()
-    page.should_not_be_success_message()
+    page.should_be_login_link()
 
-def test_guest_cant_see_success_message(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
-    page.should_not_be_success_message()
-
-@pytest.mark.xfail(reason="Негативная проверка")
-def test_message_disappeared_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.take_the_goods_to_basket()
-    page.should_be_success_message()
+    page.go_to_login_page()
